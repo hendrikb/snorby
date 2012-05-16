ffbe01c89749b5db24ae6e7ee7b907e4f3a961fd
Hendrik Bergunde <hbergunde@gmx.de>
2012-05-16 17:00:52 +0200

    initial code changes for some proper ipv6 support. changed NumericIPAddr class as necessary, changed Ip model as necessary and modified datamapper mysqladapter types as it will be requested via 'rake snorby:setup'

--- lib/snorby/dm/types.rb
+++ lib/snorby/dm/types.rb
@@ -1,10 +1,17 @@
 require 'dm-migrations/auto_migration'
 require 'dm-migrations/adapters/dm-do-adapter'
+require 'snorby/model'
+
+
+
+# require '../model/types/numeric_ip_addr'
+require '/var/www/snorby2/lib/snorby/model/types/numeric_ip_addr.rb'
 
 module DataMapper
   module Migrations
     module MysqlAdapter
         include DataObjectsAdapter
+#		include Snorby::Model::Types
         module ClassMethods
           # Default types for all data object based adapters.
           #
@@ -15,7 +22,8 @@ module DataMapper
             super.merge(
               Object   => { :primitive => 'MEDIUMTEXT'},
               DateTime => { :primitive => 'DATETIME'  },
-              Time     => { :primitive => 'DATETIME'  }
+              Time     => { :primitive => 'DATETIME'  },
+			  ::Snorby::Model::Types::NumericIPAddr => { :primitive => 'VARBINARY(16)' },
             ).freeze
           end
       end      
